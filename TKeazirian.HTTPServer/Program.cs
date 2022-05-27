1052c6bc84520d4a10a9fc6fb29be614b366cf18
Taylor Keazirian <tkeazirian@8thlight.com>
2022-05-27 11:38:57 -0500

    added test for ResponseToSend method

--- TKeazirian.HTTPServer/Program.cs
+++ TKeazirian.HTTPServer/Program.cs
@@ -50,7 +50,7 @@ public static class Program
         Console.Read();
     }
 
-    private static byte[] ResponseToSend(string response)
+    public static byte[] ResponseToSend(string response)
     {
         string[] splitString = response.Split('\r');
 
