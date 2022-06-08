c81f1728806cbe281d4561e038a0258aa5058581
Taylor Keazirian <tkeazirian@8thlight.com>
2022-06-08 11:35:26 -0500

    added in constructor for router

--- TKeazirian.HTTPServer/Router.cs
+++ TKeazirian.HTTPServer/Router.cs
@@ -2,4 +2,16 @@ namespace TKeazirian.HTTPServer;
 
 public class Router
 {
+    private string method;
+    private string path;
+    private string body;
+
+    public Router(string method, string path, string body)
+    {
+        this.method = method;
+        this.path = path;
+        this.body = body;
+    }
+
+
 }
