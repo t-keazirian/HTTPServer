b667c104e11a947ffbcf11b457d2e761aaaaaa1a
Taylor Keazirian <tkeazirian@8thlight.com>
2022-06-09 12:25:50 -0500

    reworking config

--- TKeazirian.HTTPServer/Config.cs
+++ TKeazirian.HTTPServer/Config.cs
@@ -1,16 +1,15 @@
-using System.ComponentModel.Design;
-using TKeazirian.HTTPServer;
-
 namespace TKeazirian.HTTPServer;
 
 class Config
 {
+    const string ECHOBODYPATH = "/echo_body";
+
     public Dictionary<string, string> routes = new Dictionary<string, string>();
 
     public Dictionary<string, string> RoutesDictionary(string request)
     {
         Controller controller = new Controller();
-        routes.Add("/echo_body", controller.EchoRequestBody(request));
+        routes.Add(ECHOBODYPATH, controller.EchoRequestBody(request));
         return routes;
     }
 }
