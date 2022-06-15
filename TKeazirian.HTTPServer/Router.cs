52d5de4f6d2c2e20da932c2428289f08786614af
Taylor Keazirian <tkeazirian@8thlight.com>
2022-06-15 16:49:45 -0500

    implemented dictionary in Router

--- TKeazirian.HTTPServer/Router.cs
+++ TKeazirian.HTTPServer/Router.cs
@@ -7,26 +7,35 @@ public class Router
         Controller controller = new Controller();
 
         string requestPath = Parser.ParseRequestPath(request);
+
         string requestMethod = Parser.ParseRequestMethod(request);
+
+        Dictionary<string, Dictionary<string, string>> info =
+            new Dictionary<string, Dictionary<string, string>>
+            {
+                {
+                    "POST",
+                    new Dictionary<string, string>
+                    {
+                        { "/echo_body", controller.EchoRequestBody(request) },
+                    }
+                },
+                {
+                    "GET",
+                    new Dictionary<string, string>
+                    {
+                        { "/simple_get", controller.SimpleGetNoBody(request) },
+                        { "/simple_get_with_body", controller.CreateResponseForGetRequest(request) },
+                    }
+                }
+            };
 
         try
         {
-            if (requestMethod == "POST" && requestPath == "/echo_body")
-            {
-                return controller.EchoRequestBody(request);
-            }
-
-            if (requestMethod == "GET" && requestPath == "/simple_get")
-            {
-                return controller.SimpleGetNoBody(request);
-            }
-
-            if (requestMethod == "GET" && requestPath == "/simple_get_with_body")
-            {
-                return controller.CreateResponseForGetRequest(request);
-            }
+            return info[requestMethod][requestPath];
         }
-        catch (Exception e)
+        catch
+            (Exception e)
         {
             Console.WriteLine(e);
         }
