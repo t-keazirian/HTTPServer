87a01fc121e6e3be06e2572f45b29dbd7d096e01
Taylor Keazirian <tkeazirian@8thlight.com>
2022-07-12 16:24:24 -0500

    added new acceptance tests to http_server_spec

--- http_server_spec/features/01_getting_started/method_not_allowed.feature
+++ http_server_spec/features/01_getting_started/method_not_allowed.feature
@@ -2,8 +2,15 @@
 Feature: Method Not Allowed
 
   @wip
-  Scenario: Finding GET for an endpoint with only HEAD
-    Given I make an GET request to "/head_request"
+  Scenario: Finding POST for an endpoint with only GET
+    Given I make a POST request to "/simple_get"
     Then my response should have status code 405
-    And my response should have allowed headers of HEAD, OPTIONS
+    And my response should have allowed headers of GET, HEAD, OPTIONS
+    And my response should have an empty body
+
+  @wip
+  Scenario: Finding POST for an endpoint with only GET
+    Given I make a POST request to "/simple_get_with_body"
+    Then my response should have status code 405
+    And my response should have allowed headers of GET, HEAD, OPTIONS
     And my response should have an empty body
--- http_server_spec/features/steps/01_getting_started/method_not_allowed.rb
+++ http_server_spec/features/steps/01_getting_started/method_not_allowed.rb
@@ -3,11 +3,19 @@ require "./features/steps/shared.rb"
 class Spinach::Features::MethodNotAllowed < Spinach::FeatureSteps
   include Shared::Standard
 
-  step 'I make an GET request to "/head_request"' do
-    @response = Requests.get("/head_request")
+  step 'I make a POST request to "/simple_get"' do
+    @response = Requests.get("/simple_get")
   end
 
-  step 'my response should have allowed headers of HEAD, OPTIONS' do
-    expect(@response.allowed_headers).to contain_exactly("HEAD", "OPTIONS")
+  step 'my response should have allowed headers of GET, HEAD, OPTIONS' do
+    expect(@response.allowed_headers).to contain_exactly("GET", "HEAD", "OPTIONS")
+  end
+
+  step 'I make a POST request to "/simple_get_with_body"' do
+    @response = Requests.get("/simple_get_with_body")
+  end
+
+  step 'my response should have allowed headers of GET, HEAD, OPTIONS' do
+    expect(@response.allowed_headers).to contain_exactly("GET", "HEAD", "OPTIONS")
   end
 end
