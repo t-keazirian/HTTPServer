5b649ac0036cdcf6d5435d1598f228afbd8ddfb2
Taylor Keazirian <tkeazirian@8thlight.com>
2022-07-21 15:52:03 -0500

    added logic for debug path

--- TKeazirian.HTTPServer/Handler/HealthCheckHandler.cs
+++ TKeazirian.HTTPServer/Handler/HealthCheckHandler.cs
@@ -7,12 +7,32 @@ public class HealthCheckHandler : Handler
 {
     public override Response HandleResponse(Request request)
     {
-        string body = File.ReadAllText(@"./http_server_spec/web/health-check.html");
+        string path = GetPath();
+
+        string body = File.ReadAllText(path);
         return new ResponseBuilder()
             .SetStatusCode(HttpStatusCode.Ok)
             .SetHeaders(ResponseHeaderName.ContentType, ContentType.HtmlText)
             .SetHeaders(ResponseHeaderName.ContentLength, body.Length.ToString())
             .SetBody(body)
             .Build();
+    }
+
+    private static string GetPath()
+    {
+        string path = "";
+        var currentDirectory = Directory.GetCurrentDirectory();
+        if (currentDirectory == "/Users/taylorkeazirian/Code/HTTPServer/TKeazirian.HTTPServer/bin/Debug/net6.0")
+        {
+            var workingDirectory = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString());
+            var projectDirectory = Directory.GetParent(workingDirectory.ToString());
+            path = projectDirectory + "/http_server_spec/web/health-check.html";
+        }
+        else
+        {
+            path = @"./http_server_spec/web/health-check.html";
+        }
+
+        return path;
     }
 }
